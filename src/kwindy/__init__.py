6f10f607a7cb1c4f795a8623e4c590a6b4878e84
Katie Brennan <mary.k.brennan6@gmail.com>
2024-07-29 04:27:33 +0000

    add weibull fit functions.

--- src/kwindy/__init__.py
+++ src/kwindy/__init__.py
@@ -1,4 +1,18 @@
 """kwindy: Katie's windy package."""
 
-from kwindy.statistics import calc_windspeed, calc_winddirection
 from kwindy.dataio import load_era5_from_cloud
+from kwindy.dataio import subsample_era5_variables
+from kwindy.statistics import calc_winddirection
+from kwindy.statistics import calc_windspeed
+from kwindy.statistics import fit_weibull
+from kwindy.statistics import fit_weibull_xarray
+
+
+__all__ = [
+    "load_era5_from_cloud",
+    "subsample_era5_variables",
+    "calc_winddirection",
+    "calc_windspeed",
+    "fit_weibull",
+    "fit_weibull_xarray",
+]
--- src/kwindy/statistics.py
+++ src/kwindy/statistics.py
@@ -1,16 +1,52 @@
 """Module containing statistical tools."""
 
 import numpy as np
+import xarray as xr
+from scipy.stats import weibull_min
 
 
 def calc_windspeed(u, v):
     """Calculates windspeed from u and v components."""
-    windspeed = np.sqrt(u**2+v**2)
+    windspeed = np.sqrt(u**2 + v**2)
     return windspeed
 
 
 def calc_winddirection(u, v):
     """Calculates wind direction from the u and v components."""
     wind_dir = 90 - np.rad2deg(np.arctan2(-v, -u))
-    wind_dir = wind_dir % 360 
+    wind_dir = wind_dir % 360
     return wind_dir
+
+
+def fit_weibull(data):
+    """Fit weibull distribution to data.
+    Inputs:
+        data: Array of float values to be fit
+    Returns:
+        shape: float, parameter of weibull distribution
+        loc: float, parameter of weibull distribution
+        scale: float, parameter of weibull distribution
+    """
+    shape, loc, scale = weibull_min.fit(data, floc=0)
+    return shape, loc, scale
+
+
+def fit_weibull_xarray(data, dim):
+    """Apply Weibull fit function to Xarrary DataArray.
+    Inputs:
+        data: Xarray DataArray to be fit
+        dim: string with name of dimension to fit along
+    Outputs:
+        shape: float, parameter of weibull distribution
+        loc: float, parameter of weibull distribution
+        scale: float, parameter of weibull distribution
+    """
+    return xr.apply_ufunc(
+        fit_weibull,
+        data,
+        input_core_dims=[[dim]],
+        output_core_dims=[[], [], []],
+        vectorize=True,
+        dask="parallelized",
+        output_dtypes=[float, float, float],
+    )
