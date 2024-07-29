6a9c7a4ecbf7e093546518eb73af1b2f5e835172
Katie Brennan <mary.k.brennan6@gmail.com>
2024-07-29 04:17:42 +0000

    Adding variable subselecting function for era5.

--- src/kwindy/dataio.py
+++ src/kwindy/dataio.py
@@ -1,19 +1,37 @@
 """Module for data input/output handling."""
 
-import xarray as xr 
+import xarray as xr
 
 
 def load_era5_from_cloud(desired_chunks):
     """Load ERA5 dataset from GCS.
-    Inputs: 
-        desired chunks: string or dictionary with desired chunks
+    Inputs:
+        desired_chunks: string or dictionary with desired chunks
 
-    Returns: 
+    Returns:
         full_era5_ds: xarray dataset containing full ERA5 dataset
     """
     full_era5_ds = xr.open_zarr(
-        'gs://gcp-public-data-arco-era5/ar/full_37-1h-0p25deg-chunk-1.zarr-v3',
+        "gs://gcp-public-data-arco-era5/ar/full_37-1h-0p25deg-chunk-1.zarr-v3",
         chunks=desired_chunks,
-        storage_options=dict(token='anon'),
+        storage_options=dict(token="anon"),
     )
     return full_era5_ds
+
+
+def subsample_era5_variables(full_era5_ds, variables):
+    """Subsample relevant variables from full ERA5 dataset.
+    Inputs:
+        full_era5_ds: xarray.dataset will all ERA5 variables
+        variables: list of strings with desired variable names
+
+    Return:
+        era5_subset_ds: xarray.dataset with subsampled variables
+    """
+
+    vars_to_drop = [var for var in list(full_era5_ds.data_vars) if var not in variables]
+
+    era5_subset_ds = full_era5_ds.drop_vars(vars_to_drop)
+    era5_subset_ds = era5_subset_ds.drop_vars("level")
+
+    return era5_subset_ds
